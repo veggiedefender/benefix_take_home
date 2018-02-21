4128b3ee70c9fb63d61a6f5d8d51f8385cb67e1b
veggiedefender <jessejesse123@gmail.com>
2018-02-21 18:30:20 -0500

    write rows to excel template

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const fs = require('fs');
 const path = require('path');
+const Excel = require('exceljs');
 const { parseFiles } = require('aetna_parser');
+
+const templatePath = 'template/BeneFix Small Group Plans upload template.xlsx';
+const outputPath = 'out.xlsx';
 
 async function main() {
   const pdfDir = path.join(__dirname, 'pdf');
   const pdfFiles = fs.readdirSync(pdfDir)
     .map(pdf => path.join(pdfDir, pdf));
-  console.log(await parseFiles(pdfFiles));
+  const rows = await parseFiles(pdfFiles);
+
+  let workbook = new Excel.Workbook();
+  workbook = await workbook.xlsx.readFile(path.join(__dirname, templatePath));
+  const worksheet = workbook.getWorksheet(1);
+
+  // workaround to remove empty rows
+  worksheet._rows = worksheet._rows.slice(0, worksheet.actualRowCount);
+  worksheet.addRows(rows);
+  await workbook.xlsx.writeFile(outputPath);
 }
 
 main();
